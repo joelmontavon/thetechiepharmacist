f72bea1160b300549a55ac16cef51beffb233ad7
joelmontavon <joelmontavon@users.noreply.github.com>
2022-01-03 17:27:49 -0600

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -3,30 +3,30 @@ routes.config (['$routeProvider',
   function ($routeProvider) {
 	$routeProvider.
 	  when ('/', {
-		templateUrl: '/partials/home.html'
+		templateUrl: '/thetechiepharmacist/partials/home.html'
 	  }).
 	  when('/pdc', {
 		controller: 'controller',
-		templateUrl: '/partials/pdc.html'
+		templateUrl: '/thetechiepharmacist/partials/pdc.html'
 	  }).
 	  when('/med', {
 		controller: 'controller',
-		templateUrl: '/partials/med.html'
+		templateUrl: '/thetechiepharmacist/partials/med.html'
 	  }).
 	  when('/dashboard', {
 		controller: 'dashboardController',
-		templateUrl: '/partials/dashboard.html'
+		templateUrl: '/thetechiepharmacist/partials/dashboard.html'
 	  }).
 	  when('/measures', {
 		controller: 'measuresController',
-		templateUrl: '/partials/measures.html'
+		templateUrl: '/thetechiepharmacist/partials/measures.html'
 	  }).	  
 	  when('/trends', {
 		controller: 'trendsController',
-		templateUrl: '/partials/trends.html'
+		templateUrl: '/thetechiepharmacist/partials/trends.html'
 	  }).
 	  when('/statins', {
-		templateUrl: '/partials/statins.html'
+		templateUrl: '/thetechiepharmacist/partials/statins.html'
 	  }).
 	  when('/google2cab125c9ebd6b0b.html', {
 		templateUrl: '/google2cab125c9ebd6b0b.html'
